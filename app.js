6ca35fde88ed7ac7ac7c1561331ceac2f626fa0f
alieissa <aeissa.o@gmail.com>
2023-03-25 18:49:51 +0100

    1-Generate express app Generate express app and add necessary packages to authentication using JWT and connect to DynamoDB.
    Removed all unnecessary files, packages, and pieces of
    code that were added by express generator

---
+++ app.js
@@ -0,0 +1,17 @@
+/** @format */
+
+const express = require('express')
+
+const indexRouter = require('./routes/index')
+const usersRouter = require('./routes/users')
+
+const app = express()
+
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.use('/', indexRouter)
+app.use('/users', usersRouter)
+
+module.exports = app
