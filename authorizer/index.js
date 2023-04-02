56502c45eb616d822409e8cbde654f996adcc834
alieissa <aeissa.o@gmail.com>
2023-04-02 17:55:31 +0200

    #3 - Clean up Update authorizer cicd file. It is not the main file. Remove condition to run user pipeline on push. It was there for testing

--- authorizer/index.js
+++ authorizer/index.js
@@ -3,9 +3,7 @@
 // AWS is included by default in Lambdas
 // https://aws.amazon.com/blogs/developer/why-and-how-you-should-use-aws-sdk-for-javascript-v3-on-node-js-18/#:~:text=The%20Node.,default%20in%20AWS%20Lambda%20Node
 // Only importing these to make sure imports work and nothing else
-// import AWS from 'aws-sdk'
 import * as AWS from '@aws-sdk/client-dynamodb'
-// const client = new AWS.Lambda({ region: 'REGION' })
 import { ExtractJwt } from 'passport-jwt'
 
 // https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/DynamoDB.html#constructor-property
@@ -16,7 +14,6 @@ const client = new AWS.DynamoDB({
 })
 const opts = { jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken() }
 
-export const handler = (event) => {
+export const handler = () => {
   console.log('Handler executed')
-  console.log(event)
 }
