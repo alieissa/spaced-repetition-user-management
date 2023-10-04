4355bace49e557d214b060ea0abfdc7dc24c2fd8
alieissa <aeissa.o@gmail.com>
2023-10-03 23:41:17 -0400

    5 - Clean up config files.

--- config/config.exs
+++ config/config.exs
@@ -1,10 +1,3 @@
-# This file is responsible for configuring your application
-# and its dependencies with the aid of the Config module.
-#
-# This configuration file is loaded before any dependency and
-# is restricted to this project.
-
-# General application configuration
 import Config
 
 config :users,
@@ -17,9 +10,8 @@ config :users, UsersWeb.Endpoint,
   render_errors: [
     formats: [json: UsersWeb.ErrorJSON],
     layout: false
-  ],
-  pubsub_server: Users.PubSub,
-  live_view: [signing_salt: "Nr2suYTJ"]
+  ]
+  # pubsub_server: Users.PubSub
 
 # Configures Elixir's Logger
 config :logger, :console,
@@ -28,18 +20,6 @@ config :logger, :console,
 
 config :users, :http, HTTPoison
 
-config :users, Users.Mailer,
-  adapter: Swoosh.Adapters.AmazonSES,
-  region: "us-east-1",
-  access_key: System.get_env("AWS_ACCESS_KEY"),
-  secret: System.get_env("AWS_SECRET_ACCESS_KEY")
-
-config :users, Oban,
-  repo: Users.Repo,
-  plugins: [Oban.Plugins.Pruner],
-  queues: [default: 10, registration: 10]
-
-# Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
 
 # Import environment specific config. This must remain at the bottom
--- config/runtime.exs
+++ config/runtime.exs
@@ -20,86 +20,39 @@ if System.get_env("PHX_SERVER") do
   config :users, UsersWeb.Endpoint, server: true
 end
 
-if config_env() == :prod do
-  redis_endpoint =
-    System.get_env("REDIS_ENDPOINT") ||
-    raise """
-    environment variable REDIS_ENDPOINT is missing.
-    """
+config :users, Users.Repo,
+  database: System.get_env("DB_NAME", "spaced_repetition"),
+  username: System.get_env("DB_USERNAME", "postgres"),
+  password: System.get_env("DB_PASSWORD", "postgres"),
+  hostname: System.get_env("POSTGRES_HOSTNAME"),
+  pool_size: 10
 
-  database_url =
-    System.get_env("DATABASE_URL") ||
-      raise """
-      environment variable DATABASE_URL is missing.
-      For example: ecto://USER:PASS@HOST/DATABASE
-      """
+config :users, Oban,
+  repo: Users.Repo,
+  plugins: [Oban.Plugins.Pruner],
+  queues: [default: 10, registration: 10]
 
-  maybe_ipv6 = if System.get_env("ECTO_IPV6") in ~w(true 1), do: [:inet6], else: []
+secret_key_base = System.get_env("SECRET_KEY_BASE")
 
-  config :users, Users.Repo,
-    # ssl: true,
-    url: database_url,
-    pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
-    socket_options: maybe_ipv6
+port = String.to_integer(System.get_env("PORT", "4000"))
+config :users, UsersWeb.Endpoint,
+  http: [ip: {0, 0, 0, 0}, port: port],
+  secret_key_base: secret_key_base
 
-  # The secret key base is used to sign/encrypt cookies and other secrets.
-  # A default value is used in config/dev.exs and config/test.exs but you
-  # want to use a different value for prod and you most likely don't want
-  # to check this value into version control, so we use an environment
-  # variable instead.
-  secret_key_base =
-    System.get_env("SECRET_KEY_BASE") ||
-      raise """
-      environment variable SECRET_KEY_BASE is missing.
-      You can generate one by calling: mix phx.gen.secret
-      """
+config :users, UsersWeb.Auth.Guardian,
+  issues: "users_app",
+  secret_key: secret_key_base
 
-  port = String.to_integer(System.get_env("PORT") || "4000")
+redis_endpoint = System.get_env("REDIS_ENDPOINT", "redis")
 
-  config :users, UsersWeb.Endpoint,
-    http: [
-      ip: {0, 0, 0, 0},
-      port: port
-    ],
-    secret_key_base: secret_key_base
+config :users, Redix,
+  host: redis_endpoint,
+  name: :tokens
 
-  config :users, Redix,
-      host: redis_endpoint,
-      name: :tokens
-
-  config :users, UsersWeb.Auth.Guardian,
-    issues: "users_app",
-    secret_key: secret_key_base
-
-  # ## SSL Support
-  #
-  # To get SSL working, you will need to add the `https` key
-  # to your endpoint configuration:
-  #
-  #     config :users, UsersWeb.Endpoint,
-  #       https: [
-  #         ...,
-  #         port: 443,
-  #         cipher_suite: :strong,
-  #         keyfile: System.get_env("SOME_APP_SSL_KEY_PATH"),
-  #         certfile: System.get_env("SOME_APP_SSL_CERT_PATH")
-  #       ]
-  #
-  # The `cipher_suite` is set to `:strong` to support only the
-  # latest and more secure SSL ciphers. This means old browsers
-  # and clients may not be supported. You can set it to
-  # `:compatible` for wider support.
-  #
-  # `:keyfile` and `:certfile` expect an absolute path to the key
-  # and cert in disk or a relative path inside priv, for example
-  # "priv/ssl/server.key". For all supported SSL configuration
-  # options, see https://hexdocs.pm/plug/Plug.SSL.html#configure/1
-  #
-  # We also recommend setting `force_ssl` in your endpoint, ensuring
-  # no data is ever sent via http, always redirecting to https:
-  #
-  #     config :users, UsersWeb.Endpoint,
-  #       force_ssl: [hsts: true]
-  #
-  # Check `Plug.SSL` for all available options in `force_ssl`.
+if config_env() !== :test do
+  config :users, Users.Mailer,
+    adapter: Swoosh.Adapters.AmazonSES,
+    region: System.get_env("REGION", "us-east-1"),
+    access_key: System.get_env("AWS_SES_ACCESS_KEY"),
+    secret: System.get_env("AWS_SES_SECRET_ACCESS_KEY")
 end
