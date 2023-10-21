ccc3cdce55034a5197a60aaa33d7510e4d519dee
alieissa <aeissa.o@gmail.com>
2023-10-21 19:46:31 -0400

    Use database url to connect to db

--- config/runtime.exs
+++ config/runtime.exs
@@ -20,11 +20,10 @@ if System.get_env("PHX_SERVER") do
   config :users, UsersWeb.Endpoint, server: true
 end
 
+database_url = System.get_env("DATABASE_URL")
+
 config :users, Users.Repo,
-  database: System.get_env("DB_NAME", "spaced_repetition"),
-  username: System.get_env("DB_USERNAME", "postgres"),
-  password: System.get_env("DB_PASSWORD", "postgres"),
-  hostname: System.get_env("POSTGRES_HOSTNAME"),
+  url: database_url,
   pool_size: 10
 
 config :users, Oban,
@@ -35,6 +34,7 @@ config :users, Oban,
 secret_key_base = System.get_env("SECRET_KEY_BASE")
 
 port = String.to_integer(System.get_env("PORT", "4000"))
+
 config :users, UsersWeb.Endpoint,
   http: [ip: {0, 0, 0, 0}, port: port],
   secret_key_base: secret_key_base
@@ -44,6 +44,7 @@ config :users, UsersWeb.Auth.Guardian,
   secret_key: secret_key_base
 
 redis_host = System.get_env("REDIS_HOST", "redis")
+
 config :users, Redix,
   host: redis_host,
   name: :tokens
