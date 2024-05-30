9f092c0f9e03126a3d8c378d8e71cf83aa185ca3
alieissa <aeissa.o@gmail.com>
2024-05-30 12:06:45 -0400

    Move mailer config to prod.

--- config/prod.exs
+++ config/prod.exs
@@ -1,7 +1,13 @@
 import Config
 
 # Do not print debug messages in production
-config :logger, level: :info
+config :logger, level: :debug
+
+config :users, Users.Mailer,
+  adapter: Swoosh.Adapters.AmazonSES,
+  region: System.get_env("REGION"),
+  access_key: System.get_env("AWS_SES_ACCESS_KEY"),
+  secret: System.get_env("AWS_SES_SECRET_ACCESS_KEY")
 
 # Runtime production configuration, including reading
 # of environment variables, is done on config/runtime.exs.
