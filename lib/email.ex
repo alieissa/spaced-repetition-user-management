625499bf1c710860f28d3189be8a8df44a89999d
alieissa <aeissa.o@gmail.com>
2024-06-21 22:33:40 -0400

    Clean up welcome email dispatch
    Removed token retrieval from oban job handler and separated email
    delivery from email setup.The idea is to avoid side effects, as much as
    possib, in the functions.This makes them easy to reason about. This
    change also gives us a nice separation of concern.
    
    A tag was also added to the new_user job, this allows to match on the
    tag in the perform function.This change essentially makes the flow very
    much like redux, where new_user dispatches an action  where the tag acts
    as action type and perform  acts as a reducer that updates the state
    according what the tag is.

--- lib/email.ex
+++ lib/email.ex
@@ -1,7 +1,7 @@
 defmodule Users.Email do
   import Swoosh.Email
 
-  def welcome(user: user, token: token) do
+  def welcome(email: email, token: token) do
     verification_url = System.get_env("VERIFICATION_URL")
     verification_link = "#{verification_url}?token=#{token}"
 
@@ -9,11 +9,10 @@ defmodule Users.Email do
       "Thank you for signing up. Please click <a href=#{verification_link}>here</a> to verify your email"
 
     new()
-    |> to(user.email)
+    |> to(email)
     |> from("info@spaced-reps.com")
     |> subject("spaced-reps.com signup")
     |> html_body(html_body)
     |> text_body("Please verify your email\n")
-    |> Users.Mailer.deliver()
   end
 end
--- lib/events.ex
+++ lib/events.ex
@@ -1,34 +1,15 @@
 defmodule Users.Events do
   use Oban.Worker, queue: :registration
-  require Logger
-  alias UsersWeb.Auth.Guardian
 
   @impl Oban.Worker
-  def perform(%Oban.Job{args: %{"email" => email}}) do
-    case Guardian.get_token(email) do
-      {:ok, token, user} ->
-        case Users.Email.welcome(user: user, token: token) do
-          {:ok, _} ->
-            :ok
-
-          {:error, reason} ->
-            Logger.debug(reason)
-            {:error, reason}
-        end
-
-      error ->
-        Logger.debug(error)
-        {:error, "Unable to get user token"}
-    end
-
-    # with {:ok, token, user} <- Guardian.get_token(email) do
-    #   Users.Email.welcome(user: user, token: token)
-    # end
+  def perform(%Oban.Job{args: %{"email" => email, "token" => token}, tags: ["new-user"]}) do
+    welcome = Users.Email.welcome(email: email, token: token)
+    Users.Mailer.deliver(welcome)
   end
 
-  def new_user(%{"email" => _email, "password" => _password} = user_params) do
-    user_params
-    |> new(queue: :registration)
+  def new_user(%{email: email, token: token}) do
+    %{email: email, token: token}
+    |> new(queue: :registration, tags: ["new-user"])
     |> Oban.insert()
   end
 end
