1293b8dcf1ea69276e10054f4b4ece93fc679eb8
alieissa <aeissa.o@gmail.com>
2024-06-23 18:35:57 -0400

    Set default value of verified field
    Set the default value of verified field to false.This bring the Ecto
    schema and db schema to parity.This was necessary, because when
    inserting a user the struct returned by Ecto had a null verified while
    in the database the field was false.
    
    Note that it is probably the right time to separate Account from
    User.

--- lib/users_web/controllers/user_json.ex
+++ lib/users_web/controllers/user_json.ex
@@ -24,8 +24,7 @@ defmodule UsersWeb.UserJSON do
       id: user.id,
       email: user.email,
       first_name: user.first_name,
-      last_name: user.last_name,
-      verified: user.verified
+      last_name: user.last_name
     }
   end
 end
